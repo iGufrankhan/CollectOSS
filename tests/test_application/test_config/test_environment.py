b2f99caa89e28cf79f716ab29d952bc39b72ab44
Adrian Edwards <adredwar@redhat.com>
2026-05-08 14:42:44 -0400

    basic functionality unit tests
    Signed-off-by: Adrian Edwards <adredwar@redhat.com>

--- tests/test_application/test_config/test_environment.py
+++ tests/test_application/test_config/test_environment.py
@@ -1,5 +1,6 @@
 from collectoss.application.environment import SystemEnv, extract_prefix
 import logging
+import os
 
 logger = logging.getLogger(__name__)
 
@@ -16,3 +17,23 @@ def test_env_extract_prefix_default():
 
 def test_env_extract_prefix_unprefixed():
     assert extract_prefix("DB", prefixes) is None
+
+def test_fetching_env():
+    # plain
+    os.environ["COLLECTOSS_NAME"] = "A"
+    assert SystemEnv.get("COLLECTOSS_NAME") == "A"
+
+    # fallback handling
+    os.environ["OTHER_THING"] = "B"
+    assert SystemEnv.get("COLLECTOSS_THING", None, prefixes) == "B"
+
+    # cleanup
+    del os.environ["COLLECTOSS_NAME"]
+    del os.environ["OTHER_THING"]
+
+def test_fetching_env_no_value():
+    assert SystemEnv.get("COLLECTOSS_MISSING", None, prefixes) is None
+
+def test_fetching_env_default():
+    assert SystemEnv.get("COLLECTOSS_DEFAULT", "SOME", prefixes) == "SOME"
+
