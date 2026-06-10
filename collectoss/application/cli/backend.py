f8e2d4f6ba098dc1a2b2b6d04a2256f3f2332322
Adrian Edwards <adredwar@redhat.com>
2026-06-10 18:03:18 -0400

    remove unused imports
    Signed-off-by: Adrian Edwards <adredwar@redhat.com>

--- collectoss/application/cli/backend.py
+++ collectoss/application/cli/backend.py
@@ -30,7 +30,6 @@ from collectoss.application.db.lib import get_value
 from collectoss.application.cli import test_connection, test_db_connection, with_database, DatabaseContext
 import sqlalchemy as s
 
-from collectoss.util.startup import check_init_schema, check_update_schema, collect_env_variables, print_platform_information, setup_facade_directory, warn_import_repos, merge_config
 from keyman.KeyClient import KeyClient, KeyPublisher
 
 reset_logs = SystemEnv.get_bool("AUGUR_RESET_LOGS", True)
