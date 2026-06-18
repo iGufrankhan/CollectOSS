702292788d9af252ff509857fac8c6329e8624bf
Adrian Edwards <adredwar@redhat.com>
2026-06-18 14:09:21 -0400

    make key masking a little better as as debugging tool without compromising security of keys
    Signed-off-by: Adrian Edwards <adredwar@redhat.com>

--- collectoss/util/keys.py
+++ collectoss/util/keys.py
@@ -1,5 +1,7 @@
 def mask_key(key: str, first: int = 6, last: int = 3, stars: int = 6) -> str:
     """Mask key except for the first and last few characters."""
+    if key is None:
+        return None
     if not isinstance(key, str) or len(key) <= (first + last):
-        return "*" * stars
+        return ("*" * stars) + str(type(key))
     return f"{key[:first]}{'*' * stars}{key[-last:]}"
