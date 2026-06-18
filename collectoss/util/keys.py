01938a42b0f01807702254846d73587d0bff9ff2
Adrian Edwards <adredwar@redhat.com>
2026-06-18 14:30:14 -0400

    split logic apart so its more maintainable
    Signed-off-by: Adrian Edwards <adredwar@redhat.com>

--- collectoss/util/keys.py
+++ collectoss/util/keys.py
@@ -2,6 +2,10 @@ def mask_key(key: str, first: int = 6, last: int = 3, stars: int = 6) -> str:
     """Mask key except for the first and last few characters."""
     if key is None:
         return None
-    if not isinstance(key, str) or len(key) <= (first + last):
+
+    if isinstance(key, str):
+        if len(key) <= (first + last):
+            return "*" * stars
+        return f"{key[:first]}{'*' * stars}{key[-last:]}"
+    else:
         return ("*" * stars) + str(type(key))
-    return f"{key[:first]}{'*' * stars}{key[-last:]}"
